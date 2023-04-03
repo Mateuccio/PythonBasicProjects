e79a679946cfbba8850e37b4b0330ab8622cee63
Mateuccio <bilski.mateusz@protonmail.com>
2023-04-03 11:50:15 +0200

    Python project commit

---
+++ Project 6 - Tuples/tuples.py
@@ -0,0 +1,10 @@
+#Creating tuples
+my_info = ('Mateusz', 33, 'Programmer')
+print(my_info)
+
+#Unpacking Tuple
+name, age, occupation = my_info
+print(name)
+
+#One element tiple
+one_element_tuple = (4,)
